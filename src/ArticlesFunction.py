664579f59814d47a1a1bc21022153d14fdf015d3
YueHeng <60292818+yyheng@users.noreply.github.com>
2020-11-21 02:46:28 +0800

    Update ArticlesFunction.py

--- src/ArticlesFunction.py
+++ src/ArticlesFunction.py
@@ -1,5 +1,5 @@
 import pymongo
-from datetime import datetime
+import datetime as dt
 import hashlib
 
 client = pymongo.MongoClient("mongodb+srv://admin:IBXxRxezhvT9f4D3@cluster0.vkqbl.mongodb.net/<dbname>?retryWrites=true&w=majority")
@@ -56,12 +56,23 @@ def CheckLike(db,userID,articleID):
         return False
 
 def SelectRecentArticles(db):
-    query = {}
+    date = dt.datetime.today().date()
     selectedcol = db["Articles"]
-    results = selectedcol.find(query)
-    for i in results:
-        print(i["ArticleDate"])
+    start = dt.datetime(2019, 11, 20, 0, 0, 0, 0)
+    end = dt.datetime(2020, 11, 20, 0, 0, 0, 0)
+
+    for doc in selectedcol.find({'time': {'$gte': start, '$lt': end}}):
+        print(doc)
+    return
+    #print(date.isoformat())
+    #"ArticleDate": {"$lte": "new Date()", "$gt": (date - dt.timedelta(days=1)).isoformat()}
+    query = {"ArticleDate": { "$gte": "ISODate('2019-11-20Z')", "$lt": "ISODate('2020-11-21Z')"}}
+
+    results = list(selectedcol.find(query))
     print(results)
+    # for i in results:
+    #     print(i)
+    #print(results)
     #This function is to select the past 24 hours of articles so as to generate the word cloud
     # query = "SELECT ArticleText FROM article WHERE ArticleDate >= date_sub(curdate(), interval 1 day)"
     # cursor.execute(query)
