553108719c3c86b9be779a55bdde3d121be9bfe8
BryanSim-SIT <60721725+BryanSim-SIT@users.noreply.github.com>
2020-11-28 21:19:34 +0800

    change the title to Average sentiment for agency
    ^

--- app.py
+++ app.py
@@ -310,7 +310,7 @@ def admin_dashboard():
     allAvgSentimentRating = AllAvgSentimentRating(cursor)
     values4 = []
     labels4 = []
-    legend4 = 'Agency with the most well-liked article'
+    legend4 = 'Average Sentiment for Agency'
     for x in range(len(allAvgSentimentRating)):
         labels4.append((allAvgSentimentRating[x][1]))
         values4.append(allAvgSentimentRating[x][2])
